cc74e81bfe7e06d51dbbdba557a0cd63342ccbb5
souravsahapartho <souravsahapartho@gmail.com>
2025-09-01 08:29:20 +0600

    Updated

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const validPin = 1234;
 
-
-//function to get input values (Reusable Function)
+//Shared Function
+//functions to get input values (Reusable Function)
 function getInputValueNumber(id) {
     const inputField = document.getElementById(id);
     const inputFieldValue = inputField.value;
@@ -10,16 +10,51 @@ function getInputValueNumber(id) {
     return inputFieldValueNumber;
 }
 
+function getInputValue(id) {
+    const inputField = document.getElementById(id);
+    const inputFieldValue = inputField.innerText;
+    return inputFieldValue;
+}
+
+
+//function to get innerText
+function getInnerText(id) {
+    const element = document.getElementById(id);
+    const elementValue = element.innerText;
+    const elementValueNumber = parseInt(elementValue);
+    return elementValueNumber;
+}
+
+//function to set innertext
+function setInnerText(value) {
+    const availableBalanceElement = document.getElementById('available-balance');
+    availableBalanceElement.innerText = value;
+}
+
+//function to toggle
+function handleToggle(id) {
+    const forms = document.getElementsByClassName('form');
+
+    console.log(forms);
+
+    for (const form of forms) {
+        form.style.display = "none";
+    }
+
+    document.getElementById(id).style.display = "block";
+}
+
+
 //add money button feature
 document.getElementById('add-money-btn').addEventListener('click', function (e) {
     e.preventDefault();
-    const bank = document.getElementById('bank').value;
+    const bank = getInputValue('bank');
     const accountNumber = document.getElementById('account-number').value;
     const addAmount = getInputValueNumber('add-amount');
 
     const pin = getInputValueNumber('add-pin');
 
-    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    const availableBalance = getInnerText('available-balance');
 
     if (accountNumber.length < 11) {
         alert('Please provide a valid account number');
@@ -33,7 +68,7 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
 
     const totalNewAvailableBalance = addAmount + availableBalance;
 
-    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+    setInnerText(totalNewAvailableBalance);
 })
 
 
@@ -45,7 +80,7 @@ document.getElementById('withdraw-btn').addEventListener('click', function (e) {
     const agentNumber = document.getElementById('agent-number').value;
     const CashPin = getInputValueNumber('cash-pin');
 
-    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    const availableBalance = getInnerText('available-balance');
 
     if (agentNumber.length < 11) {
         alert('Please provide a valid account number');
@@ -58,23 +93,28 @@ document.getElementById('withdraw-btn').addEventListener('click', function (e) {
     }
 
     const totalNewAvailableBalance = availableBalance - amount;
-
-    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+    setInnerText(totalNewAvailableBalance);
 
 })
+
+
 
 
 
 //toggling feature
 
-document.getElementById('add-button').addEventListener('click', function () {
-    document.getElementById('cash-out-parent').style.display = "none";
-
-    document.getElementById('add-money-parent').style.display = "block";
+document.getElementById('add-button').addEventListener('click', function (e) {
+    handleToggle('add-money-parent');
 })
 
 document.getElementById('cash-out-button').addEventListener('click', function () {
-    document.getElementById('add-money-parent').style.display = "none";
+    handleToggle('cash-out-parent')
+})
 
-    document.getElementById('cash-out-parent').style.display = "block";
+document.getElementById('transfer-button').addEventListener('click', function () {
+    handleToggle('transfer-money-parent');
+})
+
+document.getElementById('bonus-button').addEventListener('click', function () {
+    handleToggle('get-bonus-parent')
 })
